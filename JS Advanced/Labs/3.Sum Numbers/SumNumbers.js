88cf9bf88a302ab86e80c2b0f5bd688b6e1dc7fc
M.Angelova <104679767+MileneAngelova@users.noreply.github.com>
2022-09-19 23:47:13 +0300

    Delete JS Advanced/Labs/3.Sum Numbers directory

--- JS Advanced/Labs/3.Sum Numbers/SumNumbers.js
+++
@@ -1,6 +0,0 @@
-function calc() {
-    const first = document.getElementById('num1').value;
-    const second = document.getElementById('num2').value;
-    const result = Number(first) + Number(second);
-    document.getElementById('sum').value = result;
-}
