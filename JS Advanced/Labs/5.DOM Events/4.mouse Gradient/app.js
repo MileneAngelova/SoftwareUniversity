b85d5a0d96f3e2d5dfa2b5cda1c3a69f15ad52df
M.Angelova <104679767+MileneAngelova@users.noreply.github.com>
2022-10-01 12:15:54 +0300

    Delete JS Advanced/Labs/5.DOM Events/4.mouse Gradient directory

--- JS Advanced/Labs/5.DOM Events/4.mouse Gradient/app.js
+++
@@ -1,9 +0,0 @@
-function attachGradientEvents() {
-    let result = document.getElementById('result');
-    let gradient = document.getElementById('gradient');
-    gradient.addEventListener('mousemove', count);
-
-    function count(event) {
-          result.textContent = Math.floor(event.offsetX / gradient.clientWidth * 100) + '%';
-    }
-}
