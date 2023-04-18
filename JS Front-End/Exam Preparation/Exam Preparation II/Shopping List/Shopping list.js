98889b4186378eaae0fc5ad7659c1a6c0c87c1b8
M.Angelova <104679767+MileneAngelova@users.noreply.github.com>
2023-04-18 23:28:22 +0300

    Update Shopping list.js

--- JS Front-End/Exam Preparation/Exam Preparation II/Shopping List/Shopping list.js
+++ JS Front-End/Exam Preparation/Exam Preparation II/Shopping List/Shopping list.js
@@ -1,48 +1,35 @@
-function shopping(items) {
+function shopping(input) {
 
-    let list = items.shift().split('!');
-    let command = items.shift();
+   let list = input.shift().split('!');
 
-    while (command !== 'Go Shopping!') {
+    for (let token of input) {
+        let command = token.split(' ')[0];
+        let item = token.split(' ')[1];
 
-        for (let token of items) {
-
-            let instruction = token.split(' ')[0];
-
-            let item = token.split(' ')[1];
-            let itemIndex = list.indexOf(item);
-
-            if (instruction === 'Urgent') {
-                if (!list.includes(item)) {
-                    list.unshift(item);
-                }
-            } else if (instruction === 'Unnecessary') {
-                if (list.includes(item)) {
-                    list.splice(itemIndex, 1);
-                }
-            } else if (instruction === 'Correct') {
-                if (list.includes(item)) {
-                    let newItem = token.split(' ')[2];
-                    list[itemIndex] = newItem;
-                }
-            } else if (instruction === 'Rearrange') {
-                if (list.includes(item)) {
-                    list.splice(itemIndex, 1);
-                    list.push(item);
-                }
-            }
+        if (command === 'Urgent' && !list.includes(item)) {
+            list.unshift(item);
+        } else if (command === 'Unnecessary' && list.includes(item)) {
+            list.splice((list.indexOf(item)), 1);
+        } else if (command === 'Correct' && list.includes(item)) {
+            let newItem = token.split(' ')[2];
+            list[list.indexOf(item)] = newItem;
+        } else if (command === 'Rearrange' && list.includes(item)) {
+            list.splice(list.indexOf(item), 1);
+            list.push(item);
+        } else if (token === 'Go Shopping!') {
+            console.log(list.join(', '));
+            break;
         }
-        command = items.shift();
     }
-    console.log(list.join(', '));
 }
 
-shopping(["Milk!Pepper!Salt!Water!Banana",
-    "Urgent Salt",
-    "Unnecessary Water",
+shopping(["Milk!Pepper!Salt!Grapes!Banana",
+    "Urgent Salary",
+    "Unnecessary Banana",
     "Correct Pepper Onion",
-    "Rearrange Salt",
-    "Urgent Watermelon",
-    "Correct Milk Potatoes",
-    "Go Shopping!"]
+    "Urgent Banana",
+    "Correct Tomatoes Potatoes",
+    "Rearrange Onion",
+    "Go Shopping!",
+    "Urgent Watermelon"]
 );
